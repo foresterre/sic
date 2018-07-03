173397ea4d834a76429b1cc596f40cde90d1d4fc
foresterre <foresterre@ilumeo.com>
2018-07-03 22:52:56 +0200

    Completed the first try to transform the program flow to use combinators.

--- src/main.rs
+++ src/main.rs
@@ -10,26 +10,10 @@ use clap::{App, Arg};
 mod tests;
 
 // TODO{test}: add tests with images
-// TODO{code,test}: test on constraints of ICO format (256x256) before trying to convert
 // TODO{code,test}: accept different versions of supported formats
 // TODO{bug}: if an image can't be encoded, at this moment, an empty file will still be created
-
-fn image_format_from_str(format: &str) -> Result<image::ImageOutputFormat, String> {
-    let format_in_lower_case: &str = &*format.to_string().to_lowercase();
-
-    match format_in_lower_case {
-        "bmp" => Ok(image::ImageOutputFormat::BMP),
-        "gif" => Ok(image::ImageOutputFormat::GIF),
-        "ico" => Ok(image::ImageOutputFormat::ICO),
-        "jpeg" | "jpg" => Ok(image::ImageOutputFormat::JPEG(80)),
-        "png" => Ok(image::ImageOutputFormat::PNG),
-        "ppm" => Ok(image::ImageOutputFormat::PNM(
-            image::pnm::PNMSubtype::Pixmap(image::pnm::SampleEncoding::Binary),
-        )),
-        _ => Err("Image format unsupported.".to_string()),
-    }
-}
-
+// TODO{feature}: add image cropping prior to encoding (think about introducing a pipeline e.g. input -> modification -> output)
+// TODO{code}: improve error handling by defining an Error type. Possibly use the 'failure' crate.
 fn main() {
     let matches = App::new("Simple Image Converter")
         .version("0.3.0")
@@ -58,54 +42,62 @@ fn main() {
         .get_matches();
 
     // Can be unwrap because these values are required arguments.
-    let in_file_path = matches.value_of("INPUT_FILE").unwrap();
-    let out_file_path = matches.value_of("OUTPUT_FILE").unwrap();
-    println!("Provided input file: {}", in_file_path);
-    println!("Provided output file: {}", out_file_path);
+    let input = matches.value_of("INPUT_FILE").unwrap();
+    let output = matches.value_of("OUTPUT_FILE").unwrap();
+    println!("Provided input file: {}", input);
+    println!("Provided output file: {}", output);
 
     let forced_format = matches.value_of("FORCED_OUTPUT_FORMAT");
 
-    if forced_format.is_some() {
-        let image = image::open(&Path::new(in_file_path)).expect("Unable to open input image.");
-        let requested_image_format: &str = forced_format.expect("Unable to access requested image output format");
-        let image_format = image_format_from_str(requested_image_format).expect("Requested image output format not supported.");
+    let res: Result<(), String> = forced_format.map_or_else(
+        || convert_image_unforced(input, output),
+        |format| convert_image_forced(input, output, format),
+    );
 
-        let output_file = &mut File::create(&Path::new(out_file_path)).expect("Unable to save image result.");
-
-        let _ = image.write_to(output_file, image_format);
-    }
-    else {
-        match convert_image_unforced(in_file_path, out_file_path) {
-            Ok(_) => println!("Complete."),
-            Err(err) => println!("Error: {}", err),
-        }
+    match res {
+        Ok(_) => println!("Conversion complete."),
+        Err(err) => println!("Conversion ended with an Error: {}", err),
     }
 }
 
-// todo: how to deal with mutable borrows and closures
-// todo: combine forced and unforced
-// fn convert_image_forced(input: &str, output: &str, format: &str) -> Result<(), String> {
-//     image_format_from_str(format)
-//         .map_err(|err| err.to_string())
-//         .and_then(|image_format| {
-//             image::open(&Path::new(input))
-//                 .map_err(|err| err.to_string())
-//                 .map(|image| (image, image_format) )
-//         })
-//         .and_then(|(image, image_format)| {
-//             let out = &mut File::create(&Path::new(output))
-//                 .map_err(|err| err.to_string())
-//                 .map(|fout| image.write_to(&mut fout, image_format))
-//                     .map_err(|err| err.to_string());
+/// Determines the appropriate ImageOutputFormat based on a &str.
+fn image_format_from_str(format: &str) -> Result<image::ImageOutputFormat, String> {
+    let format_in_lower_case: &str = &*format.to_string().to_lowercase();
 
-//             out.map(|_| ())
-//         })
-// }
+    match format_in_lower_case {
+        "bmp" => Ok(image::ImageOutputFormat::BMP),
+        "gif" => Ok(image::ImageOutputFormat::GIF),
+        "ico" => Ok(image::ImageOutputFormat::ICO),
+        "jpeg" | "jpg" => Ok(image::ImageOutputFormat::JPEG(80)),
+        "png" => Ok(image::ImageOutputFormat::PNG),
+        "ppm" => Ok(image::ImageOutputFormat::PNM(
+            image::pnm::PNMSubtype::Pixmap(image::pnm::SampleEncoding::Binary),
+        )),
+        _ => Err("Image format unsupported.".to_string()),
+    }
+}
 
+/// Converts an image (`input`) to a certain `format` regardless of the extension of the `output` file path.
+fn convert_image_forced(input: &str, output: &str, format: &str) -> Result<(), String> {
+    image_format_from_str(format)
+        .map_err(|err| err.to_string())
+        .and_then(|image_format| {
+            image::open(&Path::new(input))
+                .map_err(|err| err.to_string())
+                .map(|image| (image, image_format))
+        })
+        .and_then(|(image, image_format)| {
+            let mut out = File::create(&Path::new(output)).map_err(|err| err.to_string())?;
+
+            image
+                .write_to(&mut out, image_format)
+                .map_err(|err| err.to_string())
+        })
+}
+
+/// Converts an image (`input`) to a certain `format` based on the extension of the `output` file path.
 fn convert_image_unforced(input: &str, output: &str) -> Result<(), String> {
     image::open(&Path::new(input))
         .map_err(|err| err.to_string())
-        .and_then(|image| image.save(output)
-            .map_err(|err| err.to_string()))
+        .and_then(|image| image.save(output).map_err(|err| err.to_string()))
 }
-
