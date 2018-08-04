073ff6965d062582c36fc6bd1aeb8cf0105726b1
foresterre <garm@ilumeo.com>
2018-08-04 11:26:56 +0200

    Format with cargo fmt.

--- src/operations/apply_operations.rs
+++ src/operations/apply_operations.rs
@@ -1,23 +1,24 @@
-use std::path::Path;
 use image::{DynamicImage, FilterType};
+use std::path::Path;
 
 use super::Operation;
 
-
 pub trait ApplyOperation {
-  fn apply_on(&self, img: &DynamicImage) -> Result<DynamicImage, String>;
+    fn apply_on(&self, img: &DynamicImage) -> Result<DynamicImage, String>;
 }
 
 impl ApplyOperation for Operation {
-  fn apply_on(&self, img: &DynamicImage) ->  Result<DynamicImage, String> {
-      match *self {
-        Operation::Blur(sigma) => Ok(img.blur(sigma as f32)),
-        Operation::FlipHorizontal => Ok(img.fliph()),
-        Operation::FlipVertical => Ok(img.flipv()),
-        Operation::Resize(new_x, new_y) => Ok(img.resize_exact(new_x, new_y, FilterType::Gaussian)),
-        _ => unimplemented!(),
-      }
-  }
+    fn apply_on(&self, img: &DynamicImage) -> Result<DynamicImage, String> {
+        match *self {
+            Operation::Blur(sigma) => Ok(img.blur(sigma as f32)),
+            Operation::FlipHorizontal => Ok(img.fliph()),
+            Operation::FlipVertical => Ok(img.flipv()),
+            Operation::Resize(new_x, new_y) => {
+                Ok(img.resize_exact(new_x, new_y, FilterType::Gaussian))
+            }
+            _ => unimplemented!(),
+        }
+    }
 }
 
 #[cfg(test)]
@@ -27,75 +28,70 @@ mod tests {
     const TEST_IMAGE_PATH: &str = "resources/unsplash_763569_cropped.jpg";
 
     fn setup() -> DynamicImage {
-      image::open(&Path::new(TEST_IMAGE_PATH)).unwrap()
+        image::open(&Path::new(TEST_IMAGE_PATH)).unwrap()
     }
 
     fn manual_inspection(img: &DynamicImage, path: &str) {
-      if !cfg!(feature = "dont-run-on-ci") {
-        let _ = img.save(path);
-      }
+        if !cfg!(feature = "dont-run-on-ci") {
+            let _ = img.save(path);
+        }
     }
 
     #[test]
     fn test_blur() {
-      let img: DynamicImage = setup();
-      let operation = Operation::Blur(25);
+        let img: DynamicImage = setup();
+        let operation = Operation::Blur(25);
 
-      let done = operation.apply_on(&img);
-      assert!(done.is_ok());
+        let done = operation.apply_on(&img);
+        assert!(done.is_ok());
 
-      manual_inspection(&done.unwrap(), "target/test_blur.png")
+        manual_inspection(&done.unwrap(), "target/test_blur.png")
     }
 
-      #[test]
+    #[test]
     fn test_fliph() {
-      let img: DynamicImage = setup();
-      let operation = Operation::FlipHorizontal;
+        let img: DynamicImage = setup();
+        let operation = Operation::FlipHorizontal;
 
-      let done = operation.apply_on(&img);
-      assert!(done.is_ok());
+        let done = operation.apply_on(&img);
+        assert!(done.is_ok());
 
-      manual_inspection(&done.unwrap(), "target/test_fliph.png")
+        manual_inspection(&done.unwrap(), "target/test_fliph.png")
     }
 
     #[test]
     fn test_flipv() {
-      let img: DynamicImage = setup();
-      let operation = Operation::FlipVertical;
+        let img: DynamicImage = setup();
+        let operation = Operation::FlipVertical;
 
-      let done = operation.apply_on(&img);
-      assert!(done.is_ok());
+        let done = operation.apply_on(&img);
+        assert!(done.is_ok());
 
-      manual_inspection(&done.unwrap(), "target/test_flipv.png")
+        manual_inspection(&done.unwrap(), "target/test_flipv.png")
     }
-
 
     #[test]
     fn test_resize_up_gaussian() {
-      // 217x447px => 400x500
-      let img: DynamicImage = setup();
-      let operation = Operation::Resize(400, 500);
+        // 217x447px => 400x500
+        let img: DynamicImage = setup();
+        let operation = Operation::Resize(400, 500);
 
-      let done = operation.apply_on(&img);
-      assert!(done.is_ok());
+        let done = operation.apply_on(&img);
+        assert!(done.is_ok());
 
-      manual_inspection(&done.unwrap(), "target/test_scale_400x500.png")
+        manual_inspection(&done.unwrap(), "target/test_scale_400x500.png")
     }
 
     #[test]
     fn test_resize_down_gaussian() {
-      // 217x447px => 100x200
-      let img: DynamicImage = setup();
-      let operation = Operation::Resize(100, 200);
+        // 217x447px => 100x200
+        let img: DynamicImage = setup();
+        let operation = Operation::Resize(100, 200);
 
-      let done = operation.apply_on(&img);
-      assert!(done.is_ok());
+        let done = operation.apply_on(&img);
+        assert!(done.is_ok());
 
-      manual_inspection(&done.unwrap(), "target/test_scale_100x200.png")
+        manual_inspection(&done.unwrap(), "target/test_scale_100x200.png")
     }
 
-
 }
-
-
-
