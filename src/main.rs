5bf77474195f8e4b2bd9d170b3bce491d419b0b0
Martijn Gribnau <garm@ilumeo.com>
2018-11-22 17:37:33 +0100

    Set program version equal to CARGO_PKG_VERSION environment variable.

--- src/main.rs
+++ src/main.rs
@@ -26,7 +26,7 @@ const HELP_OPERATIONS_AVAILABLE: &str = include_str!("../docs/cli_help_script.tx
 
 fn main() -> Result<(), String> {
     let matches = App::new("Simple Image Converter")
-        .version("0.7.2")
+        .version(env!("CARGO_PKG_VERSION"))
         .author("Martijn Gribnau <garm@ilumeo.com>")
         .about("Converts an image from one format to another.\n\n\
                 Supported input formats are described BMP, GIF, ICO, JPEG, PNG, PPM (limitations may apply). \n\n\
