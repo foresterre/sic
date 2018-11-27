f06c3c99daad0e534a37c9b4bc9e1cb6d6ccc976
Martijn Gribnau <garm@ilumeo.com>
2018-11-27 18:30:51 +0100

    Add license command test.

--- tests/cli_license.rs
+++ tests/cli_license.rs
@@ -11,9 +11,12 @@ fn run_license_command() -> Output {
 fn cli_license_full() {
     let res = run_license_command();
 
-    let parts = vec!["Simple Image Converter license: \n\n", include_str!("../LICENSE"), "\n\n\n"];
+    let parts = vec![
+        "Simple Image Converter license: \n\n",
+        include_str!("../LICENSE"),
+        "\n\n\n",
+    ];
 
     assert!(res.status.success());
-    assert_eq!(&parts.join(""), std::str::from_utf8(&res.stdout)
-        .unwrap());
+    assert_eq!(&parts.join(""), std::str::from_utf8(&res.stdout).unwrap());
 }
