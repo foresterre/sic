560d676c056b2f2319312ef70994353ee4548f2b
Martijn Gribnau <garm@ilumeo.com>
2018-11-27 18:30:39 +0100

    Add license command test.

---
+++ tests/cli_license.rs
@@ -0,0 +1,19 @@
+use std::process::{Command, Output};
+
+fn run_license_command() -> Output {
+    Command::new("cargo")
+        .args(&["run", "--", "--license"])
+        .output()
+        .expect("Running test failed")
+}
+
+#[test]
+fn cli_license_full() {
+    let res = run_license_command();
+
+    let parts = vec!["Simple Image Converter license: \n\n", include_str!("../LICENSE"), "\n\n\n"];
+
+    assert!(res.status.success());
+    assert_eq!(&parts.join(""), std::str::from_utf8(&res.stdout)
+        .unwrap());
+}
