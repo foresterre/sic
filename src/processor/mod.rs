7bcdd6426aac97ab875a70b7dc25edcc1cc8a52a
Martijn Gribnau <garm@ilumeo.com>
2019-01-10 15:07:44 +0100

    Add proof of concept for the pre image processing stage.

--- src/processor/mod.rs
+++ src/processor/mod.rs
@@ -1,4 +1,9 @@
 use crate::config::Config;
+use std::cell::Cell;
+use image::DynamicImage;
+use crate::config::FormatEncodingSettings;
+use crate::config::JPEGEncodingSettings;
+use crate::config::PNMEncodingSettings;
 
 pub(crate) mod conversion;
 pub(crate) mod encoding_format;
@@ -38,4 +43,108 @@ pub trait ProcessWithConfig<T> {
 /// Linear application pipeline trait for mutable references.
 pub trait ProcessMutWithConfig<T> {
     fn process_mut(&mut self, config: &Config) -> T;
+}
+
+// =================================
+// ========== Prototype ============
+// =================================
+
+enum Signal {
+    Continue,
+    Stop,
+    StopWithError(String)
+}
+
+struct Pipeline {
+    buffer: Cell<DynamicImage>,
+    pre_image_transform_steps: Vec<Box<Fn(&Config) -> Signal>>,
+    image_transform_steps: Vec<Box<Fn(&Cell<DynamicImage>, &Config) -> Signal>>,
+    config: Config,
+}
+
+impl Pipeline {
+    fn run(&mut self) -> Result<(), String> {
+        // current code is written to be explicit; and as a proof of concept
+
+        // pop() pops from the back
+        self.pre_image_transform_steps.reverse();
+
+        let mut step = 0;
+        let mut current = None;
+
+        while !self.pre_image_transform_steps.is_empty() {
+            current = self.pre_image_transform_steps.pop();
+            step += 1;
+
+            println!("step {}", step);
+            match current {
+                Some(f) => {
+                    let func: Box<Fn(&Config) -> Signal> = f;
+
+                    match func(&self.config) {
+                        Signal::Continue => (),
+                        Signal::Stop => return Ok(()),
+                        Signal::StopWithError(message) => return Err(message),
+                    }
+                },
+                None => return Ok(()),
+            };
+
+        }
+
+        // Completed all steps
+        Ok(())
+    }
+
+
+}
+
+
+#[test]
+fn __debug__() {
+    fn example_stop_err(config: &Config) -> Signal {
+        println!("stop with err");
+        Signal::StopWithError("stop with err".to_string())
+    }
+
+    fn example_stop(config: &Config) -> Signal {
+        println!("stop");
+        Signal::Stop
+    }
+
+    fn example_continue(config: &Config) -> Signal {
+        println!("continue");
+        Signal::Continue
+    }
+
+    let mut pipeline = Pipeline {
+        buffer: Cell::new(DynamicImage::new_luma8(2, 2)),
+        pre_image_transform_steps: vec![
+            Box::new(example_continue),
+            Box::new(example_stop),
+            Box::new(example_stop_err)
+        ],
+        image_transform_steps: vec![],
+        config: Config {
+            licenses: vec![],
+            user_manual: None,
+            script: None,
+            forced_output_format: None,
+            disable_automatic_color_type_adjustment: false,
+            encoding_settings: FormatEncodingSettings {
+                jpeg_settings: JPEGEncodingSettings {
+                    quality: 80,
+                },
+                pnm_settings: PNMEncodingSettings {
+                    ascii: true,
+                }
+            },
+            output: Some("hello_world".to_string()),
+        }
+    };
+
+    let r = pipeline.run();
+
+
+    assert_eq!(Ok(()), r);
 }
