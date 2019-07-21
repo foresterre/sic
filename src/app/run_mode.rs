2bc3a87d3c1754979e0ad30323aefbbf0678407a
Martijn Gribnau <foresterre@users.noreply.github.com>
2019-07-21 22:17:17 +0000

    Rewrite sic_io to no longer depend on sic_config. (#135)
    * Rewrite sic_io to no longer depend on sic_config.
    
    For increased portability between modules (i.e. in the event of removing or replacing the config, or dependability of other projects).

--- src/app/run_mode.rs
+++ src/app/run_mode.rs
@@ -1,9 +1,16 @@
+use std::error::Error;
+use std::path::Path;
+
 use clap::ArgMatches;
 use sic_config::Config;
-
+use sic_core::image;
 use sic_image_engine::engine::{ImageEngine, Program};
-use sic_io::encoding_format::EncodingFormatDecider;
-use sic_io::{export, import};
+use sic_io::conversion::AutomaticColorTypeAdjustment;
+use sic_io::encoding_format::{
+    DetermineEncodingFormat, EncodingFormatByIdentifier, EncodingFormatByMethod, JPEGQuality,
+};
+use sic_io::{export, import, ExportMethod, ExportSettings};
+
 use sic_user_manual::user_manual_printer::UserManualPrinter;
 
 use crate::app::custom_config::manual_arg;
@@ -31,12 +38,55 @@ pub fn run(matches: &ArgMatches, program: Program, options: &Config) -> Result<(
     )?;
 
     let mut image_engine = ImageEngine::new(img);
-    let out = image_engine
+    let buffer = image_engine
         .ignite(program)
         .map_err(|err| err.to_string())?;
 
-    let format_decider = EncodingFormatDecider::default();
-    export(out, &format_decider, &options)
+    let export_method =
+        determine_export_method(options.output.as_ref()).map_err(|err| err.to_string())?;
+
+    let encoding_format_determiner = DetermineEncodingFormat {
+        pnm_sample_encoding: if options.encoding_settings.pnm_settings.ascii {
+            Some(image::pnm::SampleEncoding::Ascii)
+        } else {
+            Some(image::pnm::SampleEncoding::Binary)
+        },
+        jpeg_quality: {
+            let quality = JPEGQuality::try_from(options.encoding_settings.jpeg_settings.quality)
+                .map_err(|err| err.to_string());
+
+            Some(quality?)
+        },
+    };
+
+    let encoding_format = match &options.forced_output_format {
+        Some(format) => encoding_format_determiner.by_identifier(format.as_str()),
+        None => encoding_format_determiner.by_method(&export_method),
+    }
+    .map_err(|err| err.to_string())?;
+
+    export(
+        buffer,
+        export_method,
+        encoding_format,
+        ExportSettings {
+            adjust_color_type: AutomaticColorTypeAdjustment::default(),
+        },
+    )
+}
+
+fn determine_export_method<P: AsRef<Path>>(
+    output_path: Option<P>,
+) -> Result<ExportMethod<P>, Box<dyn Error>> {
+    let method = if output_path.is_none() {
+        ExportMethod::StdoutBytes
+    } else {
+        let path = output_path
+            .ok_or_else(|| "The export method 'file' requires an output file path.".to_string());
+        ExportMethod::File(path?)
+    };
+
+    Ok(method)
 }
 
 pub fn run_display_licenses(config: &Config) -> Result<(), String> {
