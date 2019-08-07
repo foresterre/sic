61de945296d32c129499dd65eed15544dd2144f8
Martijn Gribnau <foresterre@users.noreply.github.com>
2019-08-07 16:07:11 +0000

    Enforce pairing cli method of input and output.  (#143)
    * Add conflicts to ensure one input method and one output method.
    * Allow the method of input / output to only be used paired with the same method for input / output.
    
    Ensure only -i and -o can be used xor INPUT_FILE and OUTPUT_FILE.
    
    * Move the cli input / output method tests to their own test module.

--- src/app/cli.rs
+++ src/app/cli.rs
@@ -6,6 +6,25 @@ use std::str::FromStr;
 
 const HELP_OPERATIONS_AVAILABLE: &str = include_str!("../../docs/cli_help_script.txt");
 
+// table of argument names
+pub(crate) mod arg_names {
+    // cli - possible arguments
+    pub(crate) const ARG_FORCED_OUTPUT_FORMAT: &str = "forced_output_format";
+    pub(crate) const ARG_LICENSE: &str = "license";
+    pub(crate) const ARG_DEP_LICENSES: &str = "dep_licenses";
+    pub(crate) const ARG_JPEG_ENCODING_QUALITY: &str = "jpeg_encoding_quality";
+    pub(crate) const ARG_PNM_ENCODING_ASCII: &str = "pnm_encoding_ascii";
+    pub(crate) const ARG_DISABLE_AUTOMATIC_COLOR_TYPE_ADJUSTMENT: &str =
+        "disable_automatic_color_type_adjustment";
+    pub(crate) const ARG_INPUT: &str = "input";
+    pub(crate) const ARG_OUTPUT: &str = "output";
+    pub(crate) const ARG_USER_MANUAL: &str = "user_manual";
+    pub(crate) const ARG_INPUT_FILE: &str = "input_file";
+    pub(crate) const ARG_OUTPUT_FILE: &str = "output_file";
+    pub(crate) const ARG_APPLY_OPERATIONS: &str = "script";
+}
+use arg_names::*;
+
 pub fn cli() -> App<'static, 'static> {
     App::new(get_tool_name())
         .version(env!("CARGO_PKG_VERSION"))
@@ -29,68 +48,74 @@ pub fn cli() -> App<'static, 'static> {
         .setting(AppSettings::NextLineHelp)
 
         // Base arguments shared between `sic` and `stew`.
-        .arg(Arg::with_name("forced_output_format")
+        .arg(Arg::with_name(ARG_FORCED_OUTPUT_FORMAT)
             .short("f")
             .long("output-format")
             .value_name("FORMAT")
             .help("Force the output image format to use FORMAT, regardless of the (if any) extension of the given output file path. \
                 Output formats (FORMAT values) supported: BMP, GIF, ICO, JPEG, PNG, PBM, PGM, PPM and PAM.")
             .takes_value(true))
-        .arg(Arg::with_name("license")
+        .arg(Arg::with_name(ARG_LICENSE)
             .long("license")
             .help("Displays the license of this piece of software (`stew`).")
             .takes_value(false))
-        .arg(Arg::with_name("dep_licenses")
+        .arg(Arg::with_name(ARG_DEP_LICENSES)
             .long("dep-licenses")
             .help("Displays the licenses of the dependencies on which this software relies.")
             .takes_value(false))
-        .arg(Arg::with_name("jpeg_encoding_quality")
+        .arg(Arg::with_name(ARG_JPEG_ENCODING_QUALITY)
             .long("jpeg-encoding-quality")
             .help("Set the jpeg quality to QUALITY. Valid values are natural numbers from 1 up to and including 100. Will only be used when the output format is determined to be jpeg.")
             .value_name("QUALITY")
             .takes_value(true))
-        .arg(Arg::with_name("pnm_encoding_ascii")
+        .arg(Arg::with_name(ARG_PNM_ENCODING_ASCII)
             .long("pnm-encoding-ascii")
             .help("Use ascii based encoding when using a PNM image output format (pbm, pgm or ppm). Doesn't apply to 'pam' (PNM Arbitrary Map)."))
-        .arg(Arg::with_name("disable_automatic_color_type_adjustment")
+        .arg(Arg::with_name(ARG_DISABLE_AUTOMATIC_COLOR_TYPE_ADJUSTMENT)
             .long("disable-automatic-color-type-adjustment")
             .help("Some image output formats do not support the color type of the image buffer prior to encoding. By default Stew tries to adjust the color type. If this flag is provided, sic will not try to adjust the color type."))
-        .arg(Arg::with_name("input")
+        .arg(Arg::with_name(ARG_INPUT)
             .long("input")
             .short("i")
             .value_name("FILE_INPUT")
             .takes_value(true)
-            .help("Input image path. When using this option, input piped from stdin will be ignored."))
-        .arg(Arg::with_name("output")
+            .help("Input image path. When using this option, input piped from stdin will be ignored.")
+            .required_unless_one(&[ARG_INPUT_FILE, ARG_LICENSE, ARG_DEP_LICENSES, ARG_USER_MANUAL])
+            .conflicts_with_all(&[ARG_INPUT_FILE, ARG_OUTPUT_FILE]))
+        .arg(Arg::with_name(ARG_OUTPUT)
             .long("output")
             .short("o")
             .value_name("FILE_OUTPUT")
             .takes_value(true)
-            .help("Output image path. When using this option, output won't be piped to stdout."))
+            .help("Output image path. When using this option, output won't be piped to stdout.")
+            .required_unless_one(&[ARG_OUTPUT_FILE, ARG_LICENSE, ARG_DEP_LICENSES, ARG_USER_MANUAL])
+            .conflicts_with_all(&[ARG_OUTPUT_FILE, ARG_INPUT_FILE]))
 
         // Selective arguments for `sic`.
-        .arg(Arg::with_name("user_manual")
+        .arg(Arg::with_name(ARG_USER_MANUAL)
             .long("user-manual")
             .short("H")
             .help("Displays help text for different topics such as each supported script operation. Run `sic -H index` to display a list of available topics.")
             .value_name("TOPIC")
             .takes_value(true))
-        .arg(Arg::with_name("script")
+        .arg(Arg::with_name(ARG_APPLY_OPERATIONS)
             .long("apply-operations")
             .short("x")
             .alias("A")
             .help(HELP_OPERATIONS_AVAILABLE)
             .value_name("OPERATIONS")
             .takes_value(true))
-        .arg(Arg::with_name("input_file")
+        .arg(Arg::with_name(ARG_INPUT_FILE)
             .help("Sets the input file")
             .value_name("INPUT_FILE")
-            .required_unless_one(&["input", "license", "dep_licenses", "user_manual"])
+            .required_unless_one(&[ARG_INPUT, ARG_LICENSE, ARG_DEP_LICENSES, ARG_USER_MANUAL])
+            .conflicts_with_all(&[ARG_INPUT, ARG_OUTPUT])
             .index(1))
-        .arg(Arg::with_name("output_file")
+        .arg(Arg::with_name(ARG_OUTPUT_FILE)
             .help("Sets the desired output file")
             .value_name("OUTPUT_FILE")
-            .required_unless_one(&["output", "license", "dep_licenses", "user_manual"])
+            .required_unless_one(&[ARG_OUTPUT, ARG_LICENSE, ARG_DEP_LICENSES, ARG_USER_MANUAL])
+            .conflicts_with_all(&[ARG_OUTPUT, ARG_INPUT])
             .index(2))
 }
 
