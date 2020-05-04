92bc32e59b7917a9c19181b9e0f1cf4da0a54eac
Martijn M.W. Gribnau <garm@ilumeo.com>
2020-05-05 01:17:44 +0200

    Add two test cases: one to keep the format, and one to change it

--- tests/cli_glob.rs
+++ tests/cli_glob.rs
@@ -1,1 +1,60 @@
+#[allow(unused)]
+#[macro_use]
+pub mod common;
 
+use common::{command, setup_output_path};
+
+#[test]
+fn keep_format() {
+    const OUT: &str = "globtest/unmodified_format";
+
+    let mut process = command("*.png", OUT, "--mode glob");
+    let result = process.wait();
+    assert!(result.is_ok());
+    assert!(result.unwrap().success());
+
+    let check_path = setup_output_path(OUT);
+
+    let count = std::fs::read_dir(check_path)
+        .unwrap()
+        .filter_map(|entry| {
+            if let Ok(entry) = entry {
+                if let Some(ext) = entry.path().extension() {
+                    if ext == "png" {
+                        return Some(ext.to_os_string());
+                    }
+                }
+            }
+            None
+        })
+        .count();
+
+    assert!(count >= 12)
+}
+#[test]
+fn modify_format() {
+    const OUT: &str = "globtest/modified_format_and_ext/";
+
+    let mut process = command("*.png", OUT, "--mode glob --output-format jpg");
+    let result = process.wait();
+    assert!(result.is_ok());
+    assert!(result.unwrap().success());
+
+    let check_path = setup_output_path(OUT);
+
+    let count = std::fs::read_dir(check_path)
+        .unwrap()
+        .filter_map(|entry| {
+            if let Ok(entry) = entry {
+                if let Some(ext) = entry.path().extension() {
+                    if ext == "jpg" {
+                        return Some(ext.to_os_string());
+                    }
+                }
+            }
+            None
+        })
+        .count();
+
+    assert!(count >= 12)
+}
