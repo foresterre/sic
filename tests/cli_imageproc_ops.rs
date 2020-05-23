ce8470f0c484e7e345193be5cc4803edaf005523
--get <garm@ilumeo.com>
2020-05-23 11:30:35 +0200

    Add integration test case for draw-text operation

---
+++ tests/cli_imageproc_ops.rs
@@ -0,0 +1,43 @@
+#![cfg(feature = "imageproc-ops")]
+
+#[macro_use]
+pub mod common;
+
+#[macro_use]
+extern crate parameterized;
+
+#[cfg(test)]
+mod tests {
+    use crate::common::*;
+
+    ide!();
+
+    #[parameterized(
+        ops = {
+            "--apply-operations \'draw-text \"example\" rgba(0,0,0,255) size(16) font(\"%font%\");\'",
+        },
+        output_file = {
+            "imageproc_ops_draw_text"
+        },
+    )]
+    fn check_imageproc_ops_with_script(ops: &str, output_file: &str) {
+        let font_file = &[
+            env!("CARGO_MANIFEST_DIR"),
+            "/resources/font/Lato-Regular.ttf",
+        ]
+        .concat();
+
+        let ops = ops.replace("%font%", font_file);
+
+        let mut process = command_with_features(
+            "unsplash_763569_cropped.jpg",
+            format!("{}.png", output_file).as_str(),
+            &ops,
+            &["imageproc-ops"],
+            false,
+        );
+        let result = process.wait();
+        assert!(result.is_ok());
+        assert!(result.unwrap().success());
+    }
+}
--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -15,16 +15,48 @@ pub fn setup_output_path(test_output_path: &str) -> PathBuf {
 
 /// In and output path prefixes are pre-defined.
 pub fn command(input: &str, output: &str, args: &str) -> Child {
+    command_with_features(input, output, args, &[], true)
+}
+
+pub fn command_with_features(
+    input: &str,
+    output: &str,
+    args: &str,
+    features: &[&str],
+    split: bool,
+) -> Child {
     let input = setup_input_path(&input);
     let input = input.to_str().unwrap();
     let output = setup_output_path(&output);
     let output = output.to_str().unwrap();
 
     let mut command = Command::new("cargo");
+    let mut arguments = Vec::with_capacity(128);
+    arguments.push("run");
 
-    let mut arguments = vec!["run", "--", "-i", input, "-o", output];
-    let provided: Vec<&str> = args.split_whitespace().collect();
+    if features.len() >= 1 {
+        arguments.push("--features");
+
+        for feature in features {
+            arguments.push(feature);
+        }
+    }
+
+    arguments.push("--");
+    arguments.push("-i");
+    arguments.push(input);
+    arguments.push("-o");
+    arguments.push(output);
+
+    let provided: Vec<&str> = if split {
+        args.split_whitespace().collect()
+    } else {
+        vec![args]
+    };
+
     arguments.extend(provided);
+
+    dbg!(&arguments);
 
     command.args(arguments);
     command.spawn().expect("Couldn't spawn child process.")
