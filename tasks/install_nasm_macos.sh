551087693ae246d55da7782113a726c4133f50cd
Martijn Gribnau <garm@ilumeo.com>
2020-11-06 07:19:22 +0100

    Add a bit of debugging info for the CI

--- tasks/install_nasm_macos.sh
+++ tasks/install_nasm_macos.sh
@@ -1,6 +1,9 @@
 export NASM_MACOS='https://www.nasm.us/pub/nasm/releasebuilds/2.15.05/macosx/nasm-2.15.05-macosx.zip'
 export NASM_MAC_PATH='nasm-2.15.05'
 
+echo "Downloading from: $NASM_MACOS"
+echo "Files path (in zip): $NASM_MAC_PATH"
+
 curl -o nasm.zip $NASM_MACOS && \
   unzip nasm.zip && \
   cd $NASM_MAC_PATH && \
