fc4f25ea7f8baec79a85269aeb02d58dc3d71a24
Martijn Gribnau <garm@ilumeo.com>
2020-11-06 08:26:28 +0100

    Attempt directly download without env
    Somehow, other ubuntu based runs are fine, but clippy isn't

--- tasks/install_nasm_linux.sh
+++ tasks/install_nasm_linux.sh
@@ -1,9 +1,3 @@
-export NASM_LINUX='https://www.nasm.us/pub/nasm/releasebuilds/2.15.05/linux/nasm-2.15.05-0.fc31.x86_64.rpm'
-export NASM_RPM='nasm-2.15.05-0.fc31.x86_64.rpm'
-
-echo "Downloading from: $NASM_LINUX"
-echo "Files path: $NASM_NASM_RPM"
-
-sudo apt install alien && \
-  wget $NASM_LINUX && \
-  sudo alien -i $NASM_RPM
+sudo apt install alien
+wget "https://www.nasm.us/pub/nasm/releasebuilds/2.15.05/linux/nasm-2.15.05-0.fc31.x86_64.rpm"
+sudo alien -i "nasm-2.15.05-0.fc31.x86_64.rpm"
