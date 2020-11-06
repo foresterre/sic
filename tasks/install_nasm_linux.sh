9c21b2cb693f4fa09e4e7b72cf375f1da02326e4
Martijn Gribnau <garm@ilumeo.com>
2020-11-06 08:32:26 +0100

    Use curl instead of wget

--- tasks/install_nasm_linux.sh
+++ tasks/install_nasm_linux.sh
@@ -1,3 +1,4 @@
 sudo apt install alien
-wget "https://www.nasm.us/pub/nasm/releasebuilds/2.15.05/linux/nasm-2.15.05-0.fc31.x86_64.rpm"
+sudo apt install curl
+curl -o "nasm-2.15.05-0.fc31.x86_64.rpm" "https://www.nasm.us/pub/nasm/releasebuilds/2.15.05/linux/nasm-2.15.05-0.fc31.x86_64.rpm"
 sudo alien -i "nasm-2.15.05-0.fc31.x86_64.rpm"
