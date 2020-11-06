0da19d9f4e37b00d33afdc6d476cddb387094ae3
Martijn Gribnau <garm@ilumeo.com>
2020-11-06 08:41:25 +0100

    Return to wget and fix clippy ci

--- tasks/install_nasm_linux.sh
+++
@@ -1,4 +0,0 @@
-sudo apt install alien
-sudo apt install curl
-curl -o "nasm-2.15.05-0.fc31.x86_64.rpm" "https://www.nasm.us/pub/nasm/releasebuilds/2.15.05/linux/nasm-2.15.05-0.fc31.x86_64.rpm"
-sudo alien -i "nasm-2.15.05-0.fc31.x86_64.rpm"
