991f4e1e8841a0e598e06993014be05744fff954
Martijn Gribnau <garm@ilumeo.com>
2020-12-23 13:32:52 +0100

    Simplify publish crate with action trait implementation

--- tasks/publish/src/pipeline/publish_crate.rs
+++ tasks/publish/src/pipeline/publish_crate.rs
@@ -1,31 +1,16 @@
 use crate::arguments::PublishWorkspace;
+use crate::pipeline::Action;
 use anyhow::Context;
 use guppy::graph::PackageMetadata;
 use std::process::Command;
 
-pub(crate) fn create_publisher<'g>(
-    pkg: PackageMetadata<'g>,
-    args: &PublishWorkspace,
-) -> anyhow::Result<Box<dyn PublishPackage + 'g>> {
-    let publish = PublishImpl::try_new(pkg, args)?;
-
-    Ok(Box::new(publish))
-}
-
-pub(crate) trait PublishPackage {
-    fn publish(&mut self) -> anyhow::Result<()>;
-}
-
-struct PublishImpl<'g> {
+pub struct PublishCrate<'g> {
     command: Command,
-    pkg: PackageMetadata<'g>,
+    pkg: &'g PackageMetadata<'g>,
 }
 
-impl<'g> PublishImpl<'g> {
-    pub(crate) fn try_new(
-        pkg: PackageMetadata<'g>,
-        args: &PublishWorkspace,
-    ) -> anyhow::Result<Self> {
+impl<'g> PublishCrate<'g> {
+    pub fn try_new(pkg: &'g PackageMetadata<'g>, args: &PublishWorkspace) -> anyhow::Result<Self> {
         let mut command = Command::new("cargo");
         command.args(&["publish", "--allow-dirty"]);
 
@@ -54,18 +39,18 @@ impl<'g> PublishImpl<'g> {
     }
 }
 
-impl PublishPackage for PublishImpl<'_> {
-    fn publish(&mut self) -> anyhow::Result<()> {
+impl Action for PublishCrate<'_> {
+    fn run(&mut self, _args: &PublishWorkspace) -> anyhow::Result<()> {
         let child_process = self.command.spawn()?;
         let result = child_process.wait_with_output()?;
 
         anyhow::ensure!(
             result.status.success(),
             format!(
-                "Cargo publish command failed for '{}' with stdout: {} and stderr: {}",
+                "Cargo publish command failed for '{}' with:\n\tstdout:\n\t{}\n\tstderr:\n\t{}",
                 self.pkg.manifest_path().display(),
-                String::from_utf8_lossy(&result.stdout),
-                String::from_utf8_lossy(&result.stderr)
+                String::from_utf8(result.stdout)?,
+                String::from_utf8(result.stderr)?,
             )
         );
 
