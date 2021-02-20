e2d7f6893a8b5380c784ebbc6faff1dfe5f3e63c
Martijn Gribnau <garm@ilumeo.com>
2021-02-20 18:00:42 +0100

    Implement flip-vertical for animated image

--- components/sic_image_engine/src/operations/flip_vertical.rs
+++ components/sic_image_engine/src/operations/flip_vertical.rs
@@ -1,6 +1,7 @@
 use crate::errors::SicImageEngineError;
 use crate::operations::ImageOperation;
-use sic_core::SicImage;
+use rayon::iter::{IntoParallelRefMutIterator, ParallelIterator};
+use sic_core::{image, SicImage};
 
 pub struct FlipVertical;
 
@@ -14,9 +15,15 @@ impl ImageOperation for FlipVertical {
     fn apply_operation(&self, image: &mut SicImage) -> Result<(), SicImageEngineError> {
         match image {
             SicImage::Static(image) => *image = image.flipv(),
-            SicImage::Animated(_) => unimplemented!(),
+            SicImage::Animated(image) => flip_vertical_animated_image(image.frames_mut()),
         }
 
         Ok(())
     }
+}
+
+fn flip_vertical_animated_image(frames: &mut [image::Frame]) {
+    frames.par_iter_mut().for_each(|frame| {
+        image::imageops::flip_vertical_in_place(frame.buffer_mut());
+    });
 }
