3691af4701196442c03c6aacc6124535fbe58123
Martijn Gribnau <garm@ilumeo.com>
2021-02-20 20:04:08 +0100

    Implement rotate180 for animated image

--- components/sic_image_engine/src/operations/rotate180.rs
+++ components/sic_image_engine/src/operations/rotate180.rs
@@ -1,6 +1,7 @@
 use crate::errors::SicImageEngineError;
 use crate::operations::ImageOperation;
-use sic_core::SicImage;
+use rayon::iter::{IntoParallelRefMutIterator, ParallelIterator};
+use sic_core::{image, SicImage};
 
 pub struct Rotate180;
 
@@ -14,9 +15,15 @@ impl ImageOperation for Rotate180 {
     fn apply_operation(&self, image: &mut SicImage) -> Result<(), SicImageEngineError> {
         match image {
             SicImage::Static(image) => *image = image.rotate180(),
-            SicImage::Animated(_) => unimplemented!(),
+            SicImage::Animated(image) => rotate180_animated_image(image.frames_mut()),
         }
 
         Ok(())
     }
+}
+
+fn rotate180_animated_image(frames: &mut [image::Frame]) {
+    frames.par_iter_mut().for_each(|frame| {
+        *frame.buffer_mut() = image::imageops::rotate180(frame.buffer_mut());
+    });
 }
