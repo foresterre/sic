a1439ebb0bb3015bb021412fbe4c3141096a23e0
Martijn Gribnau <garm@ilumeo.com>
2021-02-20 17:56:12 +0100

    Implement filter3x3 for animated image

--- components/sic_image_engine/src/operations/filter3x3.rs
+++ components/sic_image_engine/src/operations/filter3x3.rs
@@ -1,6 +1,7 @@
 use crate::errors::SicImageEngineError;
 use crate::operations::ImageOperation;
-use sic_core::SicImage;
+use rayon::iter::{IntoParallelRefMutIterator, ParallelIterator};
+use sic_core::{image, SicImage};
 
 pub struct Filter3x3<'kernel> {
     kernel: &'kernel [f32; 9],
@@ -16,9 +17,15 @@ impl<'kernel> ImageOperation for Filter3x3<'kernel> {
     fn apply_operation(&self, image: &mut SicImage) -> Result<(), SicImageEngineError> {
         match image {
             SicImage::Static(image) => *image = image.filter3x3(self.kernel),
-            SicImage::Animated(_) => unimplemented!(),
+            SicImage::Animated(image) => filter3x3_animated_image(image.frames_mut(), self.kernel),
         }
 
         Ok(())
     }
+}
+
+fn filter3x3_animated_image(frames: &mut [image::Frame], kernel: &[f32; 9]) {
+    frames.par_iter_mut().for_each(|frame| {
+        *frame.buffer_mut() = image::imageops::filter3x3(frame.buffer_mut(), kernel);
+    });
 }
