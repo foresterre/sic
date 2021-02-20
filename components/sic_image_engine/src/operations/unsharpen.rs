87294652530ac3722fa7fd8928069e7401f350bb
Martijn Gribnau <garm@ilumeo.com>
2021-02-20 20:08:00 +0100

    Implement unsharpen for animated image

--- components/sic_image_engine/src/operations/unsharpen.rs
+++ components/sic_image_engine/src/operations/unsharpen.rs
@@ -1,6 +1,7 @@
 use crate::errors::SicImageEngineError;
 use crate::operations::ImageOperation;
-use sic_core::SicImage;
+use rayon::iter::{IntoParallelRefMutIterator, ParallelIterator};
+use sic_core::{image, SicImage};
 
 pub struct Unsharpen {
     sigma: f32,
@@ -17,9 +18,17 @@ impl ImageOperation for Unsharpen {
     fn apply_operation(&self, image: &mut SicImage) -> Result<(), SicImageEngineError> {
         match image {
             SicImage::Static(image) => *image = image.unsharpen(self.sigma, self.threshold),
-            SicImage::Animated(_) => unimplemented!(),
+            SicImage::Animated(image) => {
+                unsharpen_animated_image(image.frames_mut(), self.sigma, self.threshold)
+            }
         }
 
         Ok(())
     }
+}
+
+fn unsharpen_animated_image(frames: &mut [image::Frame], sigma: f32, threshold: i32) {
+    frames.par_iter_mut().for_each(|frame| {
+        *frame.buffer_mut() = image::imageops::unsharpen(frame.buffer_mut(), sigma, threshold);
+    });
 }
