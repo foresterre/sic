d4614b0fef7a484035a05b15d252fa453794fe6c
Martijn Gribnau <garm@ilumeo.com>
2021-02-20 04:00:07 +0100

    Implement brighten for animated image

--- components/sic_image_engine/src/operations/brighten.rs
+++ components/sic_image_engine/src/operations/brighten.rs
@@ -1,6 +1,7 @@
 use crate::errors::SicImageEngineError;
 use crate::operations::ImageOperation;
-use sic_core::SicImage;
+use rayon::iter::{IntoParallelRefMutIterator, ParallelIterator};
+use sic_core::{image, SicImage};
 
 pub struct Brighten {
     amount: i32,
@@ -16,9 +17,15 @@ impl ImageOperation for Brighten {
     fn apply_operation(&self, image: &mut SicImage) -> Result<(), SicImageEngineError> {
         match image {
             SicImage::Static(image) => *image = image.brighten(self.amount),
-            SicImage::Animated(_) => unimplemented!(),
+            SicImage::Animated(image) => brighten_animated_image(image.frames_mut(), self.amount),
         }
 
         Ok(())
     }
+}
+
+fn brighten_animated_image(frames: &mut [image::Frame], amount: i32) {
+    frames.par_iter_mut().for_each(|frame| {
+        *frame.buffer_mut() = image::imageops::brighten(frame.buffer_mut(), amount);
+    });
 }
