21cc1ef2dfdf017decb4c376fdc096614a627602
Martijn Gribnau <garm@ilumeo.com>
2021-02-20 20:04:02 +0100

    Implement rotate90 for animated image

--- components/sic_image_engine/src/operations/rotate90.rs
+++ components/sic_image_engine/src/operations/rotate90.rs
@@ -1,6 +1,7 @@
 use crate::errors::SicImageEngineError;
 use crate::operations::ImageOperation;
-use sic_core::SicImage;
+use rayon::iter::{IntoParallelRefMutIterator, ParallelIterator};
+use sic_core::{image, SicImage};
 
 pub struct Rotate90;
 
@@ -14,9 +15,15 @@ impl ImageOperation for Rotate90 {
     fn apply_operation(&self, image: &mut SicImage) -> Result<(), SicImageEngineError> {
         match image {
             SicImage::Static(image) => *image = image.rotate90(),
-            SicImage::Animated(_) => unimplemented!(),
+            SicImage::Animated(image) => rotate90_animated_image(image.frames_mut()),
         }
 
         Ok(())
     }
+}
+
+fn rotate90_animated_image(frames: &mut [image::Frame]) {
+    frames.par_iter_mut().for_each(|frame| {
+        *frame.buffer_mut() = image::imageops::rotate90(frame.buffer_mut());
+    });
 }
