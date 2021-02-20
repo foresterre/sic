6a23e4ee99e831f1e1f90c1f1cd213daa2b3ad29
Martijn Gribnau <garm@ilumeo.com>
2021-02-20 18:07:17 +0100

    Implement grayscale for animated image

--- components/sic_image_engine/src/operations/grayscale.rs
+++ components/sic_image_engine/src/operations/grayscale.rs
@@ -1,6 +1,8 @@
 use crate::errors::SicImageEngineError;
 use crate::operations::ImageOperation;
-use sic_core::SicImage;
+use rayon::iter::{IntoParallelRefMutIterator, ParallelIterator};
+use sic_core::image::buffer::ConvertBuffer;
+use sic_core::{image, SicImage};
 
 pub struct Grayscale;
 
@@ -14,9 +16,15 @@ impl ImageOperation for Grayscale {
     fn apply_operation(&self, image: &mut SicImage) -> Result<(), SicImageEngineError> {
         match image {
             SicImage::Static(image) => *image = image.grayscale(),
-            SicImage::Animated(_) => unimplemented!(),
+            SicImage::Animated(image) => grayscale_animated_image(image.frames_mut()),
         }
 
         Ok(())
     }
+}
+
+fn grayscale_animated_image(frames: &mut [image::Frame]) {
+    frames.par_iter_mut().for_each(|frame| {
+        *frame.buffer_mut() = image::imageops::grayscale(frame.buffer_mut()).convert();
+    });
 }
