1793e2048a0f7e3bdf88d76adc21f128067c878b
Mustafa <mustafasalih1991@gmail.com>
2021-03-12 04:08:46 +0300

    added blending functionality to horizontal-gradient

--- components/sic_image_engine/src/operations/horizontal_gradient.rs
+++ components/sic_image_engine/src/operations/horizontal_gradient.rs
@@ -3,7 +3,7 @@ use crate::operations::ImageOperation;
 use crate::wrapper::gradient_input::GradientInput;
 use rayon::iter::{IntoParallelRefMutIterator, ParallelIterator};
 use sic_core::{
-    image::{self, DynamicImage},
+    image::{self, DynamicImage, Frame, Pixel},
     SicImage,
 };
 
@@ -30,14 +30,44 @@ impl ImageOperation for HorizontalGradient {
 }
 
 fn gradient_static_image(img: &mut DynamicImage, input: GradientInput) {
-    let right_color = &input.colors().0;
-    let left_color = &input.colors().1;
-    image::imageops::horizontal_gradient(img, right_color, left_color);
+    let (left_color, right_color) = &input.colors();
+    let (width, height) = img.to_rgba8().dimensions();
+    let mut gradient_dynamic = DynamicImage::new_rgba8(width, height);
+    image::imageops::horizontal_gradient(&mut gradient_dynamic, left_color, right_color);
+    blend_static_image(img, gradient_dynamic);
 }
 
 fn gradient_animated_image(frames: &mut [image::Frame], input: GradientInput) {
-    let colors = input.colors();
+    let (left_color, right_color) = &input.colors();
+    let (width, height) = DynamicImage::ImageRgba8(frames[0].buffer().clone())
+        .to_rgba8()
+        .dimensions();
+    let mut gradient_dynamic = DynamicImage::new_rgba8(width, height);
+    image::imageops::horizontal_gradient(&mut gradient_dynamic, left_color, right_color);
+    let gradient_buffer = gradient_dynamic.to_rgba8();
+
     frames.par_iter_mut().for_each(|frame| {
-        image::imageops::horizontal_gradient(frame.buffer_mut(), &colors.0, &colors.1);
+        blend_frame(frame, &gradient_buffer);
     });
+}
+
+fn blend_frame(frame: &mut Frame, layer: &image::ImageBuffer<image::Rgba<u8>, Vec<u8>>) {
+    let mut gradient_iter = layer.pixels();
+    let mut blended_buffer = DynamicImage::ImageRgba8(frame.buffer().clone()).into_rgba8();
+
+    blended_buffer
+        .pixels_mut()
+        .for_each(|pixel| pixel.blend(gradient_iter.next().unwrap()));
+    *frame = Frame::new(blended_buffer);
+}
+
+fn blend_static_image(img: &mut DynamicImage, layer: DynamicImage) {
+    let gradient_rgba = layer.to_rgba8();
+    let mut gradient_iter = gradient_rgba.pixels();
+    let mut blended_buffer = img.to_rgba8();
+
+    blended_buffer
+        .pixels_mut()
+        .for_each(|pixel| pixel.blend(gradient_iter.next().unwrap()));
+    *img = DynamicImage::ImageRgba8(blended_buffer);
 }
